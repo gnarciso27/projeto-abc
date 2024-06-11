4c7921b411f873c24386cc2bf9fcfbd30f2e5b56
Marcosa Trigueiro <marcosandre77@gmail.com>
2024-06-11 15:28:35 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let historia = document.querySelector('.menu__link.historia');
+let submenu = document.querySelector('.submenu');
+
+historia.onclick = function() {
+    submenu.classList.toggle('ativo');
+    console.log("ativo")
+}
